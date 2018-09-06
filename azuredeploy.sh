43606c707872def4f6f825075e2934f50dfbafc3
Dwai Banerjee <dwaiba@users.noreply.github.com>
2018-09-06 12:35:39 +0200

    Update azuredeploy.sh

--- azuredeploy.sh
+++ azuredeploy.sh
@@ -1169,13 +1169,16 @@ systemctl start docker
 }
 install_tf_kubectl_helm()
 {
-  
+if is_master; then
       su -c "curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get > $SHARE_DATA/get_helm.sh" $HPC_USER
       su -c "chmod 700 $SHARE_DATA/get_helm.sh" $HPC_USER
       su -c "$SHARE_DATA/./get_helm.sh" $HPC_USER
       su -c "/usr/local/bin/helm init --client-only --upgrade " $HPC_USER
       su -c "/usr/local/bin/helm repo add azure-samples https://azure-samples.github.io/helm-charts/  && /usr/local/bin/helm repo add gitlab https://charts.gitlab.io/  && /usr/local/bin/helm repo add ibm-charts https://raw.githubusercontent.com/IBM/charts/master/repo/stable/ && /usr/local/bin/helm  repo add bitnami https://charts.bitnami.com/bitnami" $HPC_USER
       curl -LO --retry 3 https://releases.hashicorp.com/terraform/0.11.8/terraform_0.11.8_linux_amd64.zip && unzip terraform_0.11.8_linux_amd64.zip && chmod +x ./terraform && sudo mv terraform /usr/local/bin/ && curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl && chmod +x ./kubectl && sudo mv ./kubectl /usr/local/bin/kubectl
+else
+echo "install_tf_kubectl_helm already installed on share"
+fi     
 }
 #########################
 	if [ "$skuName" == "16.04-LTS" ] ; then
@@ -1240,11 +1243,7 @@ install_tf_kubectl_helm()
 		setup_hpc_user
 		gpasswd -a $HPC_USER docker
 		setup_env
-	    is_master()
-	    {
-		    install_tf_kubectl_helm
-	    }
-
+                install_tf_kubectl_helm
 		    if [ "$SALTSTACKBOOLEAN" == "Yes" ] ; then
 		    install_saltsaltstack_centos
 		    fi
